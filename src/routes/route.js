15f9d0cadaceb0f80d0b6c4219db65cd82d231b6
Chandranath Gupta <chandranathgupta1807@gmail.com>
2022-12-17 08:03:14 +0530

    Updated-PathVsQuery

--- src/routes/route.js
+++ src/routes/route.js
@@ -160,4 +160,57 @@ router.post( "/post-query-2", function (req, res){
 })
 
 
+                                        // assignment QUERY PARAMS
+/* ASSIGNMENT:
+you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+also return an array consisting of only the person that can vote
+
+WRITE A POST API TO THE ABOVE
+
+
+take this as sample for array of persons:   */
+
+let persons= [
+   {
+   name: "PK",
+   age: 10,
+   votingStatus: false
+},
+{
+   name: "SK",
+   age: 20,
+   votingStatus: false
+},
+{
+   name: "AA",
+   age: 70,
+   votingStatus: false
+},
+{
+   name: "SC",
+   age: 5,
+   votingStatus: false
+},
+{
+   name: "HO",
+   age: 40,
+   votingStatus: false
+}
+]
+router.post('/queryParams', function(req,res){
+    let Input = req.query.age
+    let emptyArray = [];
+
+    for(let i=0; i<persons.length; i++){
+         if(persons[i].age >= Input){
+            persons[i].votingStatus = true
+            emptyArray.push(persons[i])
+         }
+    }
+    res.send({data : emptyArray})
+})
+
+
+
 module.exports = router;
